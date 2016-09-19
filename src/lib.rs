c08dc68ccb0c5d0d3b19f6819ca5a6e423b0651d
Tyrel Richey <ty.mu.tech.industries@gmail.com>
2016-09-19 14:32:33 -0600

    SNGIG

--- src/lib.rs
+++ src/lib.rs
@@ -270,7 +270,7 @@ pub fn sha256_of_message_as_u8_vec( message: &mut Vec<u8> ) -> Box<Vec<u8>> {
 }
 
 #[test]
-pub fn test_sha256_of_message_as_u8_vec() {
+fn test_sha256_of_message_as_u8_vec() {
 	// From "Mu Shall Rise! And with it so shall too, sweet liberty echo through the chamber halls, both high and low, and big and small."
 	let mut message: Vec<u8> = vec![77, 117, 32, 83, 104, 97, 108, 108, 32, 82, 105, 115, 101, 33, 32, 65, 110, 100, 32, 119, 105, 116, 104, 32, 105, 116, 32, 115, 111, 32, 115, 104, 97, 108, 108, 32, 116, 111, 111, 44, 32, 115, 119, 101, 101, 116, 32, 108, 105, 98, 101, 114, 116, 121, 32, 101, 99, 104, 111, 32, 116, 104, 114, 111, 117, 103, 104, 32, 116, 104, 101, 32, 99, 104, 97, 109, 98, 101, 114, 32, 104, 97, 108, 108, 115, 44, 32, 98, 111, 116, 104, 32, 104, 105, 103, 104, 32, 97, 110, 100, 32, 108, 111, 119, 44, 32, 97, 110, 100, 32, 98, 105, 103, 32, 97, 110, 100, 32, 115, 109, 97, 108, 108, 46];
 	let result: Vec<u8> = *sha256_of_message_as_u8_vec(&mut message);
